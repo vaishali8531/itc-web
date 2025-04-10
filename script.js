4f4ced96545332bd52170aa1888289958d01c005
vaishali8531 <vaishalikarande2006@gmail.com>
2025-04-11 03:10:48 +0530

    Delete script.js

--- script.js
+++
@@ -1,9 +0,0 @@
-document.addEventListener("DOMContentLoaded", () => {
-  const form = document.querySelector("form");
-
-  form.addEventListener("submit", function (e) {
-    e.preventDefault(); // Stop the form from refreshing the page
-    alert("Thank you! Your message has been submitted.");
-    form.reset(); // Clear the form
-  });
-});
